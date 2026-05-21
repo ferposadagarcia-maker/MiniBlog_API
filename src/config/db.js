d14cc5f6dcbc333c4a22530a2e956bd515f00f0d
Fernanda Posada <ferposadagarcia@gmail.com>
2026-05-21 01:45:29 -0600

    Configuracion final de conexión

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const isProduction = process.env.DATABASE_URL && !process.env.DATABASE_URL.includes('localhost');
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction 
-    ? { rejectUnauthorized: false } 
-    : false,
+  ssl: {
+     rejectUnauthorized: false } 
 });
 
 pool.on('connect', () => {
